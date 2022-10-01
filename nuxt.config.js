902ae1be25768443a81403a6227377f580fddfcf
Mikael Vejdemo-Johansson <mikael@johanssons.org>
2022-09-30 22:07:08 -0400

    Styled and ready. Kinda.

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,25 @@ import { defineNuxtConfig } from "nuxt";
 export default defineNuxtConfig({
     target: "static",
     ssr: true,
-    modules: ['@nuxt/content'],
+    modules: ['@nuxt/content', "@nuxtjs/tailwindcss"],
     content: {
         documentDriven: true
     },
+    vuestic: {
+        css: true,
+        fonts: true
+    },
     runtimeConfig: {
-        githubApiToken: process.env.GITHUB_API_TOKEN
+        githubApiToken: process.env.GITHUB_API_TOKEN,
+        public: {
+            owner: "michiexile",
+            homepage: "https://mikael.johanssons.org",
+            cv: "https://cv.mikael.johanssons.org",
+            scholar: "http://scholar.google.com/citations?hl=en&user=XJN1TGIAAAAJ",
+            twitter: "https://twitter.com/michiexile",
+            facebook: "https://facebook.com/michiexile",
+            instagram: "https://instagram.com/michiexile"
+        }
     }
 })
 
