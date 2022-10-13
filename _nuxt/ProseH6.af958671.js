a87e80acecfdc3880a19a2a05680c4d726ef0d29
michiexile <michiexile@users.noreply.github.com>
2022-10-13 03:37:35 +0000

    deploy: 635c9a634784119bc446c82f33d9d15e910a7514

---
+++ _nuxt/ProseH6.af958671.js
@@ -0,0 +1,1 @@
+import{Q as o,o as r,e as s,R as t}from"./entry.67cddf9e.js";const n={};function c(e,a){return r(),s("h6",null,[t(e.$slots,"default")])}const f=o(n,[["render",c]]);export{f as default};
