9582b7cd3930da66a0b2b84831f8df4fd95e1b06
michiexile <michiexile@users.noreply.github.com>
2022-10-19 03:38:43 +0000

    deploy: 635c9a634784119bc446c82f33d9d15e910a7514

---
+++ _nuxt/ProseBlockquote.b9c810cc.js
@@ -0,0 +1,1 @@
+import{Q as o,o as t,e as r,R as c}from"./entry.b486cf91.js";const s={};function n(e,l){return t(),r("blockquote",null,[c(e.$slots,"default")])}const f=o(s,[["render",n]]);export{f as default};
