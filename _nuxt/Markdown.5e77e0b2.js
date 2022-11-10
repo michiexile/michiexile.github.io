2a07c524156b191ba6aba45d81a01c151316c115
michiexile <michiexile@users.noreply.github.com>
2022-11-10 03:18:12 +0000

    deploy: 635c9a634784119bc446c82f33d9d15e910a7514

--- _nuxt/Markdown.5e77e0b2.js
+++
@@ -1,1 +0,0 @@
-import r from"./ContentSlot.cedc7236.js";import{z as o,A as u,a as f,y as c}from"./entry.1b30e20d.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
