2015550744bbffb3a927955fe68aa47f09d0928f
michiexile <michiexile@users.noreply.github.com>
2023-03-09 03:00:53 +0000

    deploy: 6c153fbd773733e1741f5d47caf4ecdbeba2eccf

---
+++ _nuxt/ProseUl.13cbd506.js
@@ -0,0 +1,1 @@
+import{V as o,o as r,e as s,W as t}from"./entry.ea672042.js";const n={};function c(e,l){return r(),s("ul",null,[t(e.$slots,"default")])}const f=o(n,[["render",c]]);export{f as default};
