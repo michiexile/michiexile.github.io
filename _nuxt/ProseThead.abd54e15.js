15247d9a67850b4c12b8a9fff0e4fe67a06887ff
michiexile <michiexile@users.noreply.github.com>
2023-06-21 02:50:14 +0000

    deploy: b927083de3841771a61acaf74ee83180483d500a

--- _nuxt/ProseThead.abd54e15.js
+++
@@ -1,1 +0,0 @@
-import{W as o,o as r,e as t,X as s}from"./entry.072ef80a.js";const a={};function n(e,c){return r(),t("thead",null,[s(e.$slots,"default")])}const d=o(a,[["render",n]]);export{d as default};
---
+++ _nuxt/composables.acb606db.js
@@ -0,0 +1,1 @@
+import{p}from"./entry.8a01c9c7.js";function s(e,u){return p()._useHead(e,u)}export{s as u};
