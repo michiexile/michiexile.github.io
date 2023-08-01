88357073555bdc8d7c26e4b071829ecfae269ad2
michiexile <michiexile@users.noreply.github.com>
2023-08-01 02:49:37 +0000

    deploy: e77ffc0d70bb127f3de0d46adecc20a1caed70bf

--- _nuxt/ProseThead.89c91816.js
+++
@@ -1,1 +0,0 @@
-import{v as o,o as r,f as s,s as t}from"./entry.1956ccc2.js";const a={};function n(e,c){return r(),s("thead",null,[t(e.$slots,"default")])}const f=o(a,[["render",n]]);export{f as default};
